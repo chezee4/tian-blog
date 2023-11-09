6e8c019f687196e59bef718ce212f06ef807917c
chezee4 <vadimdufinec@gmail.com>
2023-11-09 21:51:59 +0200

    select fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     
+      <h4 className=" font-gatham text-[100px] inline-block">Home Link Skills </h4>
+      <h1 className=" font-anton text-[100px] max-w-[300px] inline-block">PRODUCT DESIGNER CRISTIAN MUÑOZ</h1>
+      <h1 className=" font-poppins text-[100px] max-w-[300px] inline-block">PRODUCT DESIGNER CRISTIAN MUÑOZ</h1>
     </main>
   );
 }
